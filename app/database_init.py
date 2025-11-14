253ce2760f318bf549dca38cd01abc528c1f3175
Amitha Reddy Basu <amithareddy@Amithas-MacBook-Pro.local>
2025-11-13 20:16:34 -0500

    initial commit

--- app/database_init.py
+++ app/database_init.py
@@ -1,5 +1,6 @@
 from app.database import engine
-from app.models.user import Base
+from app.schemas.base import Base  
+from app.models import user       
 
 def init_db():
     Base.metadata.create_all(bind=engine)
@@ -8,4 +9,4 @@ def drop_db():
     Base.metadata.drop_all(bind=engine)
 
 if __name__ == "__main__":
-    init_db() # pragma: no cover
+    init_db()  # pragma: no cover
